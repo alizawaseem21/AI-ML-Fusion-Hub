9c8cbb48572172153ee1cc17033287ba1d2ef928
Abuzar <107863500+Abuzar-King@users.noreply.github.com>
2023-10-19 21:19:23 +0500

    Create DataFrame.py
    This is my first attempt to contribute in the open source so its just a small contribution from my side

---
+++ DataFrame.py
@@ -0,0 +1,7 @@
+# This is my first contribution for open source
+import pandas as pd
+data = {'Name': ['Arsalan', 'Arham', 'Basit', 'Dania', 'Eman', 'Saleem'],
+        'Age': [21, 20, 24, 22, 25, 49],
+        'City': ['Lahore', 'Sheikhupura', 'Faisalabad', 'Islamabad', 'Karachi', 'Lahore']}
+df = pd.DataFrame(data)
+print(df)
